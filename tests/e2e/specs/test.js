42dd6037246c0f1a8d466aa6b0f076049816b6df
Wade <wsw0615@gmail.com>
2020-07-13 14:35:14 +0800

    feat: first e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -6,3 +6,14 @@ describe('My First Test', () => {
     cy.contains('h1', 'Todos')
   })
 })
+
+describe('應顯示程式標題與輸入欄位', () => {
+  it('Visits the app root url', () => {
+    cy.visit('http://localhost:8080')
+    cy.wait(500)
+    cy.get('#app')
+    cy.get('.header')
+    cy.contains('h1', 'Todos')
+    cy.get('.new-todo')
+  })
+})
