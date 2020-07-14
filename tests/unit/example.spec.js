ab1d361742e77522ac6b71d4930a7258bad618ba
Wade <wsw0615@gmail.com>
2020-07-14 11:07:27 +0800

    feat: first unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import store from '@/components/store.js'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
@@ -10,3 +11,18 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.text()).toMatch(msg)
   })
 })
+
+describe('Store', () => {
+  it('應可從 localStorage 中取得資料', () => {
+    // arrange
+    const STORAGE_KEY = 'vue-todomvc';
+    const expected = ['test'];
+    localStorage.setItem(STORAGE_KEY, '["test"]');
+
+    // act
+    const actual = store.fetch();
+
+    // assert
+    expect(actual).toEqual(expected);
+  });
+});
